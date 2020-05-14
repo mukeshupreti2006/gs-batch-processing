e324a25fb687fb444ea09ab081486ba570d168fb
Mukesh <mukeshupreti2006@gmail.com>
2020-05-14 15:40:50 -0700

    updated  pom for h2 db to be persistence

--- complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
+++ complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -1,9 +1,6 @@
 package com.example.batchprocessing;
 
-import javax.sql.DataSource;
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,13 +12,12 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 // tag::setup[]
 @Configuration
@@ -34,6 +30,8 @@ public class BatchConfiguration {
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
 	// end::setup[]
+
+
 
 	// tag::readerwriterprocessor[]
 	@Bean
